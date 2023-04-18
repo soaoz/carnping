5f300d6e7649564d60cb2ae3e4d3fb080ef3a446
Park SoYoung <1004661@gmail.com>
2023-04-18 12:36:48 +0900

    고친버전
    ㅇㅇ

--- carnping/src/main/java/com/kh/carnping/member/controller/MemberController.java
+++ carnping/src/main/java/com/kh/carnping/member/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.kh.carnping.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
+	@RequestMapping("myProfile.me")
+	public String myProfile() {
+		return "common/footer";
+	}
 
 }
+	
---
+++ carnping/src/main/java/com/kh/carnping/member/model/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.kh.carnping.member.model.service;
+
+public interface MemberService {
+
+}
