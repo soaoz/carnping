6ba684f3ba2a68773bd9525c3bf83889e60a011f
chasingg <choihs@hotmail.com>
2023-04-19 10:33:20 +0900

    230419 member commit
    ajax테스트 용 아이디 중복확인

--- carnping/src/main/java/com/kh/carnping/member/model/service/MemberServiceImpl.java
+++ carnping/src/main/java/com/kh/carnping/member/model/service/MemberServiceImpl.java
@@ -1,5 +1,21 @@
 package com.kh.carnping.member.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kh.carnping.member.model.dao.MemberDao;
+
 public class MemberServiceImpl {
+
+	@Autowired
+	private MemberDao mDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	public int idCheck(String checkId) {
+		return mDao.idCheck(sqlSession, checkId);
+	}
 
 }
