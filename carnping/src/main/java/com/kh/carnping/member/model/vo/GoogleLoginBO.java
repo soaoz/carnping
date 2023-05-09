928d9ff9850bc479938d191ba6acb01c88b9a0d1
chasingg <choihs@hotmail.com>
2023-05-09 09:29:49 +0900

    230509 소셜로그인 구현완료
    230509 소셜로그인 구현완료

--- carnping/src/main/java/com/kh/carnping/member/model/vo/GoogleLoginBO.java
+++ carnping/src/main/java/com/kh/carnping/member/model/vo/GoogleLoginBO.java
@@ -1,6 +1,4 @@
 package com.kh.carnping.member.model.vo;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.TokenResponseException;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
@@ -8,8 +6,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.plus.Plus;
-import com.google.api.services.plus.model.Person;
 
 import java.io.IOException;
 import java.security.SecureRandom;
@@ -23,12 +19,10 @@ public class GoogleLoginBO {
     private static final String CLIENT_SECRET = "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";
     private static final String REDIRECT_URI = "http://localhost:8282/carnping/callbackGoogle.do";
 
-//    private static final List<String> SCOPES = Arrays.asList("openid", "email", "profile", "phonenumbers");
     private static final List<String> SCOPES = Arrays.asList(
             "https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email",
     		"https://www.googleapis.com/auth/user.phonenumbers.read");
-//    private static final List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile");
 
     private static final GoogleAuthorizationCodeFlow FLOW = new GoogleAuthorizationCodeFlow.Builder(
             new NetHttpTransport(),
@@ -66,21 +60,5 @@ public class GoogleLoginBO {
         return url.build();
     }
 
-//    public static String getUserEmail(String code, HttpSession session) throws IOException {
-//        String state = (String) session.getAttribute("state");
-//        GoogleTokenResponse tokenResponse = null;
-//        try {
-//            tokenResponse = FLOW.newTokenRequest(code)
-//                    .setRedirectUri(REDIRECT_URI)
-//                    .execute();
-//        } catch (TokenResponseException e) {
-//            throw new IOException("Failed to retrieve token: " + e.getMessage());
-//        }
-//        Credential credential = FLOW.createAndStoreCredential(tokenResponse, null);
-//        Plus plus = new Plus.Builder(new NetHttpTransport(), new JacksonFactory(), credential)
-//                .setApplicationName("Google Login Example")
-//                .build();
-//        Person profile = plus.people().get("me").execute();
-//        return profile.getEmails().get(0).getValue();
-//    }
+
 }
